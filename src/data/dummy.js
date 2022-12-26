137aed3c80f5ab73e2daacedf015ec810c7081d5
lawonyaungchyi1 <nyanlintun225nlt@gmail.com>
2022-12-26 09:35:12 +0630

    First

---
+++ src/data/dummy.js
@@ -0,0 +1,593 @@
+export const zoology = [
+  {
+    id: 1,
+    questions: "Capillaries are classified as",
+    selections: [
+      "Conducting vessels",
+      "Distributing vessels",
+      "Exchange vessels",
+      "Reservoir vessels",
+      "Resistance vessels",
+    ],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 2,
+    questions: "Side branches of terminal arterioles are called",
+    selections: [
+      "Capillaries",
+      "Cavernous tissue",
+      "Meta-arterioles",
+      "Sinusoids",
+      "Venules",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 3,
+    questions: "True valves present at",
+    selections: ["Femoral vein", "IVC", "Portal vein", "Pulmonary vein", "SVC"],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 4,
+    questions: "Conducting arteries include",
+    selections: [
+      "artery found in muscle",
+      "pulmonary artery",
+      "recurrent artery",
+      "renal artery",
+      "superior mesenteric artery",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 5,
+    questions: "Anatomical end artery",
+    selections: [
+      "artery that anastomose with neighboring arteries",
+      "arteries supplying the heart are such type of artery",
+      "in occlusion resulting in serious nutritional disturbance resulting in death of tissue",
+      "is found in anterior cerebral artery",
+      "arteries supplying the kidney are such type of artery",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 6,
+    questions: "Arteries",
+    selections: [
+      "all carry oxygenated blood",
+      "diameter is larger than those of corresponding veins",
+      "have pulsation",
+      "have valve",
+      "the smallest of which has a diameter of more than 100u",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 7,
+    questions: "Capillaries",
+    selections: [
+      "allow the passage of red blood cells in single file",
+      "do not anastomose each other",
+      "form the communication link between arterioles and cavernous tissues",
+      "have diameter which are 5u or less",
+      "their walls act as permeable membrane",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 8,
+    questions: "Capillaries can be found in",
+    selections: [
+      "Cornea",
+      "epidermis",
+      "external genitalia",
+      "hyaline cartilage",
+      "spleen",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 9,
+    questions: "Continuous (non-fenestrated) capillaries",
+    selections: [
+      "are abundant in renal glomeruli",
+      "are found in endocrine gland",
+      "have no basement membrane",
+      "have a diameter similar to that of a red blood corpuscle",
+      "have sympathetic innervations",
+    ],
+    correctAnswers: ["(e)"],
+  },
+  {
+    id: 10,
+    questions: "Sinusoids are described by each of the following",
+    selections: [
+      "they are lined by endothelial cells",
+      "they have no phagocytic elements",
+      "they have regular lumen",
+      "lining epithelium is continuous",
+      "lined by cuboidal cells",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 11,
+    questions:
+      "During cardiac catheterization, the physician watches the blood flow from the right ventricle into which of the following vessels?",
+    selections: [
+      "aorta",
+      "coronary arteries",
+      "inferior vena cava",
+      "pulmonary trunk",
+      "superior vena cava",
+    ],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 12,
+    questions:
+      "Which of the lists of structures include all of the central venous system?",
+    selections: [
+      "cerebellum , cerebrum , midbrain , brainstem , spinal cord",
+      "cerebellum , cerebellum , spinal nerves , cranial nerves",
+      "cerebrum , sensory neurones , motor neurones , cerebellum",
+      "midbrain , cerebellum , special sense organs , medulla oblongata",
+      "midbrain , spinal cord , autonomic nerves , pons",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 13,
+    questions:
+      "Which of the following parts of the nervous system carries impulses towards the brain?",
+    selections: [
+      "Autonomic nervous system",
+      "Parasympathetic division",
+      "Peripheral nervous system",
+      "Somatic nervous system",
+      "Sympathetic  division",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 14,
+    questions:
+      "The somatic nervous system is divided into two divisions.What are they called?",
+    selections: [
+      "Afferent and efferent",
+      "Central and peripheral",
+      "Sensory and motor",
+      "Somatic and autonomic",
+      "Sympathetic and parasympathetic",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 15,
+    questions:
+      "What do `sympathetic` and `parasympathetic` divisions refer to?",
+    selections: [
+      "Autonomic nervous system",
+      "Central nervous system",
+      "Efferent neurons of the peripheral nervous system",
+      "Peripheral nervous system",
+      "Somatic nervous system",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 16,
+    questions: "Which of the following is true of parasympathetic neurons?",
+    selections: [
+      "They all release Norepinephrine as a neurotransmitter",
+      "They all release Norepinephrine as a neurotransmitter",
+      "They emerge from the thoracic and lumbar vertebrae",
+      "They have short pre-ganglionic fibres and long post-ganglionic fibres",
+      "They have widespread action",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 17,
+    questions:
+      "A patient experiencing a central nervous system(CNS) inflammatory process will be activating which of the following phagocytic glial cells",
+    selections: [
+      "Astrocytes",
+      "Ependymal cells",
+      "Microglia",
+      "Oligodendrocytes",
+      "Schwann cells",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 18,
+    questions:
+      "The brain and spinal cord are surrounded by membranous connective tissue layers.The pain associated with most CNS inflammatory processes is mediated by sensory nerves in which of these tissue layers? ",
+    selections: [
+      "Arachnoid mater",
+      "Dura mater",
+      "Endoneurium",
+      "Ependyma",
+      "Pia mater",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 19,
+    questions:
+      "A neurologist is concerned about a patient's inability to walk without a distinct limp (movement disorder).Which of the following portions of the peripheral nervous system (PNS) will the neurologist examine first?",
+    selections: ["Autonomic", "Enteric", "Myenteric", "Somatic", "Submucosal"],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 20,
+    questions:
+      "In response to a perceived threat of danger , which of the following PNS components will be globally activated?",
+    selections: [
+      "Enteric",
+      "Parasympathetic",
+      "Postganglionic",
+      "Preganglionic",
+      "Sympathetic",
+    ],
+    correctAnswers: ["(e)"],
+  },
+  {
+    id: 21,
+    questions:
+      "The hormone that control the level of calcium and phosphorous in blood is secreted by",
+    selections: [
+      "Parathyroid gland",
+      "Pineal gland",
+      "Pituitary gland",
+      "Thymus",
+      "Thyroid gland",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 22,
+    questions: "Normal human spermatozoa",
+    selections: [
+      "has 46 chromosomes",
+      "motility at emission must be more than 80%",
+      "survival in female genital tract is 24hrs",
+      "the normal sperm count is 10 million per ml of sperm",
+      "the normal length is 165u",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 23,
+    questions: "Regarding spermatogenesis in man",
+    selections: [
+      "occur in epididymis",
+      "one spermatogonuim gives rise to four spermatids",
+      "spermatogenesis begins at birth",
+      "spermatogonia divide by meiosis",
+      "the whole process takes about 64 days",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 24,
+    questions:
+      "In spermiogenesis there are many differentiation process occurred.The following morphological change of male germ cell is correct.",
+    selections: [
+      "develop only head",
+      "gradually become enlarge",
+      "increase cytoplasm",
+      "loss a few cytoplasm",
+      "loss all cytoplasm",
+    ],
+    correctAnswers: ["(e)"],
+  },
+  {
+    id: 25,
+    questions:
+      "There are approximately (-) million sperms in the average ejaculate of 3.5 cc.",
+    selections: ["50", "100", "150", "200", ">200"],
+    correctAnswers: ["(e)"],
+  },
+  {
+    id: 26,
+    questions:
+      "In male , the primodial germ cells remain cells remain dominant in the seminiferous tubules of the testes.Which of the following time is beginning of spermatogenesis?",
+    selections: [
+      "at birth",
+      "at puberty",
+      "before birth",
+      "before puberty till death",
+      "in childhood",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 27,
+    questions:
+      "Meiosis is the cell division that takes place in the germ cells to generate male/female gametes.Which of the following cells undergoes meiosis?",
+    selections: [
+      "Oogonia",
+      "Primordial germ cells",
+      "Primary spermatocyte",
+      "Spermatogonia",
+      "Spermatids",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 28,
+    questions: "Ovulation is",
+    selections: [
+      "Expulsion of oocyte from graffian follicle",
+      "Expulsion of oocyte from primary follicle",
+      "Expulsion of oocyte only",
+      "Expulsion of oocyte with theca interna",
+      "Expulsion of oocyte with theca externa",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 29,
+    questions:
+      "Following ovulation , the granulosa cells remaining the wall of ruptured follicle , together with cells from the theca interna are vascularized by surrounding vessels.Under influence of hormone these cells develop a yellowish pigment and change into luteal cell.Which of the following is called in luteal cell of pregnancy?",
+    selections: [
+      "corpus albican",
+      "corpus atreticum",
+      "corpus callosum",
+      "corpus gravidarum",
+      "corpus luteum",
+    ],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 30,
+    questions: "Oogenesis",
+    selections: [
+      "a primary oocyte develops into 4 mature oocytes & 3 polar bodies",
+      "begins after birth",
+      "1st meiotic division is completed only if the oocyte is fertilized",
+      "no primary oocytes are formed after birth",
+      "primary oocytes are formed after birth in female",
+    ],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 31,
+    questions: "Concerning with the ova",
+    selections: [
+      "are released by the rupture of the corona radiata at the time of ovulation",
+      "are development from the primitive germ cells which are embedded in the substance of the ovaries",
+      "give rise to oogonia",
+      "measure about 2mm in diameter",
+      "undergo routine further development after liberation",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 32,
+    questions: "The human oocyte",
+    selections: [
+      "at ovulation is smallest than the human sperm",
+      "commences its first meiotic division at the age of puberty",
+      "contains a haploid number of chromosomes as a secondary oocyte",
+      "develops from the germinal epithelium of the ovary",
+      "primary oocytes finish their 1st meiotic division before puberty",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 33,
+    questions: "The formation of human oogonia ceases at:",
+    selections: [
+      "birth",
+      "menopause",
+      "puberty",
+      "3rd month of foetal development",
+      "4th week of embryonic development",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 34,
+    questions:
+      "In a woman with a 35-day menstrual cycle , ovulation could be expected to occur on or about day:",
+    selections: ["7", "14", "21", "28", "30"],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 35,
+    questions:
+      "The ovum is surrounded by a non-cellular , secreted layer known as",
+    selections: [
+      "corona radiata",
+      "cumulus oophorus",
+      "stratum granulosum",
+      "theca folliculi",
+      "zona pellucida",
+    ],
+    correctAnswers: ["(e)"],
+  },
+  {
+    id: 36,
+    questions: "Ovulation",
+    selections: [
+      "can be detected by sudden constant superolateral pain",
+      "can be inhibited by giving gonadotrophins",
+      "is induced by FSH from the anterior pituitary gland",
+      "occurs approximately 14 + of - 1 day before the onset of next menstruation",
+      "occurs 14 days after the onset of next menstruation",
+    ],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 37,
+    questions: "Corpus gravidarum",
+    selections: [
+      "is found in female virgin",
+      "is found in post-menopausal women",
+      "it produces progesterone",
+      "it forms in menstrual phase",
+      "lasts for 5 months",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 38,
+    questions:
+      "Fertilization is the process of fusion of male and female gamete.Which changes occur after fertilization?",
+    selections: [
+      "acrosome reaction",
+      "capacitation",
+      "completion of first meiotic division",
+      "formation of zygote",
+      "restoration of haploid number of chromosomes",
+    ],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 39,
+    questions:
+      "A young couple come to fertility center for their family planning.They want to consult pre-selection of their boy baby and to know what time of their baby's sex.Which of the following time is sex determination of embryo takes place?",
+    selections: [
+      "division of primary oocyte",
+      "fertilization",
+      "gametogenesis",
+      "implantation of ovum",
+      "ovulation",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 40,
+    questions: "Cleavage divisions are always",
+    selections: ["amitotic", "anucleitic", "amniotic", "meiotic", "mitotic"],
+    correctAnswers: ["(e)"],
+  },
+  {
+    id: 41,
+    questions:
+      "At the end of first week of early development the blastocyst burrowed and embedded.Which of the following site is most likely site of implantation?",
+    selections: [
+      "in the ampullary region of tube",
+      "in posterior or anterior wall of body of uterus",
+      "in the ovary",
+      "in the recto-uterine pouch",
+      "in the intestine",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 42,
+    questions: "What does capacitation mean in male gametes:",
+    selections: [
+      "activation to enter & fertilize the ovum ",
+      "power to swim in female mucous",
+      "power to alienate in direction of fallopian tubes",
+      "none of above",
+      "all of above",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 43,
+    questions:
+      "Regarding the zona pellucida all of the following statements are true except",
+    selections: [
+      "during the zona reaction, it becomes impermeable to entry of other sperms",
+      "it is associated with all mammalian oocytes",
+      "it is secreted by the oocyte & the granulosa cells",
+      "it disappears a day after fertilization",
+      "it forms a barrier permeable to large macromolecules , between the oocyte & granulosa cells",
+    ],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 44,
+    questions: "Fertilization of human sex cells is believed to occur in",
+    selections: [
+      "cervix",
+      "lower third of the oviduct",
+      "middle third of the oviduct",
+      "uterus",
+      "upper third of the oviduct",
+    ],
+    correctAnswers: ["(e)"],
+  },
+  {
+    id: 45,
+    questions: "Which of the following is normal site of implantation?",
+    selections: [
+      "abdominal cavity",
+      "internal os",
+      "near the fundus",
+      "ovary",
+      "uterine tube",
+    ],
+    correctAnswers: ["(c)"],
+  },
+  {
+    id: 46,
+    questions: "Sexuality is established in the time of:",
+    selections: [
+      "fertilization",
+      "gametogenesis",
+      "implantation",
+      "morphogenesis",
+      "ovulation",
+    ],
+    correctAnswers: ["(a)"],
+  },
+  {
+    id: 47,
+    questions: "Formation of Germ layers Trophoblast forms",
+    selections: [
+      "amnioblast",
+      "cytotrophoblast",
+      "embryoblast",
+      "epiblast",
+      "hypoblast",
+    ],
+    correctAnswers: ["(b)"],
+  },
+  {
+    id: 48,
+    questions: "The embryoblast",
+    selections: [
+      "give rise to blastomere",
+      "gives rise to placenta",
+      "gives rise to cytotrophoblast",
+      "gives rise to epiblast and hypoblast",
+      "gives rise to syncytiotrophoblast",
+    ],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 49,
+    questions: "The amniotic cavity develops:",
+    selections: [
+      "in extraembryonic mesoderm",
+      "on the tenth day",
+      "on the thirteenth day",
+      "within the epiblast near the cytotrophoblast",
+      "within the hypoblast",
+    ],
+    correctAnswers: ["(d)"],
+  },
+  {
+    id: 50,
+    questions: "Secondary yolk sac:",
+    selections: [
+      "endoderm gives rise to epithelium of the trachea , bronchi , lungs and digestive tract",
+      "ectoderm gives rise to epithelium of the trachea , bronchi , lungs and digestive tract",
+      "give rise to skin , bone and cartilage",
+      "is a new cavity formed within the amniotic cavity",
+      "lies above the epiblast of germ disc",
+    ],
+    correctAnswers: ["(a)"],
+  },
+];
